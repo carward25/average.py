9d3ed310c69a215e7ebb7a55d40659ee36300e79
carward25 <carward@ucsc.edu>
2021-10-06 16:29:30 -0700

    #This is assignment 2 part a

---
+++ average.py
@@ -0,0 +1,16 @@
+# The following program finds the average of five numbers for the user
+
+# these are inputs
+print("Please enter five numbers")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+num5 = int(input("Enter the fifth number: "))
+
+# calculation
+add = (num1 + num2 + num3 + num4 + num5)
+average = add/5
+
+# print average value
+print("The average of those numbers is:", average)
